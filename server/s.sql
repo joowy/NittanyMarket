b194589c4e9c144c5a0b2a8bb5dbc64bd8db4527
Joseph Stout <46057052+joowy@users.noreply.github.com>
2022-03-17 11:11:49 -0400

    sql script

---
+++ server/s.sql
@@ -0,0 +1,12 @@
+
+SELECT * from address;
+SELECT * from Users;
+    
+ SELECT name FROM sqlite_master WHERE type = "table";
+
+PRAGMA table_info(Users);
+PRAGMA table_info(Buyers);
+PRAGMA table_info(Sellers);
+PRAGMA table_info(Categories);
+PRAGMA table_info(Local_Vendor);
+PRAGMA table_info(Product_Listings);
