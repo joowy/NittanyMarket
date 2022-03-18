3d3228d06dc7d840cf95cb4b73a7132d4c7cd7ee
Joseph Stout <46057052+joowy@users.noreply.github.com>
2022-03-18 14:13:18 -0400

    db

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -19,108 +19,108 @@ import { login } from "features/auth";
 const theme = createTheme();
 
 export const Login = () => {
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const { isLoggedIn, user } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+    const { isLoggedIn, user } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
 
-  console.log(isLoggedIn, user);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setLoading(true);
-    const data = new FormData(event.currentTarget);
-    const { email, password } = {
-      //   email: data.get("email"),
-      //   password: data.get("password"),
-      email: "arubertelli0@nsu.edu",
-      password: "TbIF16hoUqGl",
-      //   email: "test@test.com",
-      //   password: "pass",
+    console.log(isLoggedIn, user);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setLoading(true);
+        const data = new FormData(event.currentTarget);
+        const { email, password } = {
+            email: data.get("email"),
+            password: data.get("password"),
+            //   email: "arubertelli0@nsu.edu",
+            //   password: "TbIF16hoUqGl",
+            //   email: "test@test.com",
+            //   password: "pass",
+        };
+
+        // @ts-ignore
+        dispatch(login({ email, password }))
+            // @ts-ignore
+            .unwrap()
+            .then(() => {
+                window.location.reload();
+            })
+            .catch(() => {
+                setLoading(false);
+            });
     };
 
-    // @ts-ignore
-    dispatch(login({ email, password }))
-      // @ts-ignore
-      .unwrap()
-      .then(() => {
-        window.location.reload();
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Log in
-            </Button>
-            <Grid container>
-              {/* <Grid item xs>
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Log in
+                    </Typography>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ mt: 1 }}
+                    >
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Log in
+                        </Button>
+                        <Grid container>
+                            {/* <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
                 </Link>
               </Grid> */}
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Register"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+                            <Grid item>
+                                <Link href="/register" variant="body2">
+                                    {"Don't have an account? Register"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 };
