165d7f11182b5ace0f1c3f3da730117967b2c3c2
Joseph Stout <46057052+joowy@users.noreply.github.com>
2022-03-19 04:00:35 -0400

    register fields

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth";
+import authReducer from "../slices/auth";
 const reducer = {
   auth: authReducer,
 };
